7781478d2e327f6e37132fb009c29c513c244775
t-koyama <mayatake29@gmail.com>
2015-04-07 22:10:24 +0900

    first commit

---
+++ Assets/egg_move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class egg_move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//xac = 1f * Input.acceleration.x;
+		GetComponent<Rigidbody2D>().AddForce(Vector2.up*Input.acceleration.x, ForceMode2D.Impulse);
+		//transform.Rotate();
+	}
+}
